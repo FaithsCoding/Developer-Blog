d8aee014f935858937eed537625be44be1ee370d
Faith Meades <122907573+FaithsCoding@users.noreply.github.com>
2023-05-14 20:42:00 +0100

    Fix sequelize connection file

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,13 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_HOST,
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: "mysql",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
   }
 );
 
